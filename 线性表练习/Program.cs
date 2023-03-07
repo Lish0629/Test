dcd9d44121a1932962af18d10a11cabe34441536
Shenghao Li <li.sh0629@outlook.com>
2023-03-07 12:35:03 +0800

    添加项目文件。

--- 线性表练习/Program.cs
+++ 线性表练习/Program.cs
@@ -35,15 +35,33 @@ namespace program
             else
                 return "空串";
         }
+        public void Delsame(string same)
+        {
+            int j = 0, i;
+            for ( i = 0; i < length; i++)
+            {
+                if (data[i] != same)
+                {
+                    data[j] = data[i];
+                    j++;
+                }
+                else { 
+                }
+            }
+            length -= i - j;
+        }
     }
     class program
     {
         static void Main(string[] args)
         {
             listclass listclass = new listclass();
-            string[] split = { "a","b","c","d" };
+            string[] split = { "a","b","c","d","c","b","d","e","f","b"};
             listclass.createlist(split);
-            Console.WriteLine(listclass.displist());
+            Console.WriteLine(listclass.displist()+" "+listclass.length);
+            listclass.Delsame("b");
+            Console.WriteLine(listclass.displist() + " " + listclass.length);
+
         }
     }
 
